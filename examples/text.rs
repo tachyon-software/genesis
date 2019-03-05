e8de99aa57869fd420244c8745832a7b2b2de44f
haze <isnt@haze.cool>
2019-03-05 14:36:02 -0500

    Add Examples

---
+++ examples/text.rs
@@ -0,0 +1,29 @@
+/// Example stolen from [here](https://github.com/seanmonstar/pretty-env-logger/blob/master/examples/log.rs)
+extern crate genesis;
+#[macro_use]
+extern crate log;
+
+mod nested {
+    pub fn deep() {
+        trace!("test trace");
+    }
+}
+
+fn main() {
+    genesis::init_config(
+        genesis::ConfigurationBuilder::default()
+            .display_mode(genesis::DisplayMode::Text)
+            .build()
+            .unwrap(),
+    )
+    .expect("Failed to init genesis logger");
+    if !log_enabled!(log::Level::Trace) {
+        eprintln!("To see the full demo, try setting `RUST_LOG=basic=trace`.");
+        return;
+    }
+    self::nested::deep();
+    debug!("test debug");
+    info!("test info");
+    warn!("test warn");
+    error!("test error");
+}
